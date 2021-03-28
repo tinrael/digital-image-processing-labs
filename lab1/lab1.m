78bb88f832ab82bfec27d1b3677a205dde466ac0
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-28 22:28:33 +0300

    Implement the 2D median filter with a rectangle window

---
+++ lab1/lab1.m
@@ -0,0 +1,31 @@
+clc;
+clear;
+
+originalImage = imread('saturn2.gif');
+filteredImage = applyRectangleMedianFilter(originalImage, 3, 3);
+
+imshowpair(originalImage, filteredImage, 'montage');
+
+%{
+    The window of this 2D median filter is a rectangle (a matrix which has 'numOfRows' rows and 'numOfColumns' columns).
+    This function supposes 'numOfRows' and 'numOfColumns' to be odd positive integers (not necessarily the same).
+%}
+function filteredImage = applyRectangleMedianFilter(originalImage, numOfRows, numOfColumns)
+    filteredImage = zeros(size(originalImage, 1) + 2, size(originalImage, 2) + 2);
+    filteredImage(2:(end - 1), 2:(end - 1)) = originalImage;
+    
+    offsetX = numOfColumns - ceil(numOfColumns / 2);
+    offsetY = numOfRows - ceil(numOfRows / 2);
+    
+    for i = ceil(numOfRows / 2):(size(filteredImage, 1) - offsetY)
+        for j = ceil(numOfColumns / 2):(size(filteredImage, 2) - offsetX)
+            window = filteredImage((i - offsetY):(i + offsetY), (j - offsetX):(j + offsetX));
+            
+            v = reshape(window, 1, []);
+            v = sort(v);
+            
+            middleIndex = ceil(size(v, 2) / 2);
+            filteredImage(i, j) = v(middleIndex);
+        end
+    end
+end
