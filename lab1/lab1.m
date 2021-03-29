6d47e241ffe468a169da8caecafd06548aa06277
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-03-29 18:57:56 +0300

    Implement the 2D median filter with a cross window

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -40,3 +40,46 @@ function filteredImage = applyRectangleMedianFilter(originalImage, numOfRows, nu
         end
     end
 end
+
+%{
+    The window of this 2D median filter is a cross.
+    The cross has the 'crossHeight' height and the 'crossWidth' width.
+    This function supposes 'crossHeight' and 'crossWidth' to be positive integers (not necessarily the same).
+%}
+function filteredImage = applyCrossMedianFilter(originalImage, crossHeight, crossWidth)
+    offsetX = crossWidth - ceil(crossWidth / 2);
+    offsetY = crossHeight - ceil(crossHeight / 2);
+    
+    differenceOffsetX = 0;
+    differenceOffsetY = 0;
+    
+    if rem(crossWidth, 2) == 0
+        differenceOffsetX = 1;
+    end
+    
+    if rem(crossHeight, 2) == 0
+        differenceOffsetY = 1;
+    end
+    
+    filteredImage = zeros(size(originalImage, 1) + 2 * offsetY - differenceOffsetY, size(originalImage, 2) + 2 * offsetX - differenceOffsetX);
+    filteredImage(ceil(crossHeight / 2):(end - offsetY), ceil(crossWidth / 2):(end - offsetX)) = originalImage;
+    
+    for i = ceil(crossHeight / 2):(size(filteredImage, 1) - offsetY)
+        for j = ceil(crossWidth / 2):(size(filteredImage, 2) - offsetX)
+            % the elements on the cross's horizontal line including the (i, j)
+            horizontal = filteredImage(i, (j - (offsetX - differenceOffsetX)):(j + offsetX));
+            
+            % the elements on the cross's vertical line above the (i, j) (not including (i, j))
+            above = transpose(filteredImage((i - (offsetY - differenceOffsetY)):(i - 1), j));
+            
+            % the elements on the cross's vertical line below the (i, j) (not including (i, j))
+            below = transpose(filteredImage((i + 1):(i + offsetY), j));
+            
+            cross = [horizontal above below];
+            cross = sort(cross);
+            
+            middleIndex = ceil(size(cross, 2) / 2);
+            filteredImage(i, j) = cross(middleIndex);
+        end
+    end
+end
