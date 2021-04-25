292662af7df98b9a10b6b0061f5d3a678f44d366
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-04-25 23:37:43 +0300

    Implement the edge detection algorithm

---
+++ lab2/lab2.m
@@ -0,0 +1,38 @@
+clc;
+clear;
+
+syms f(x, y) a b c alpha beta gamma
+f(x, y) = a*x^2 + b*y^2 + c*x*y + alpha*x + beta*y + gamma;
+
+syms g(x, y)
+epsilon = (f(x-1, y-1) - g(x-1, y-1))^2 + (f(x-1, y) - g(x-1, y))^2 + (f(x-1, y+1) - g(x-1, y+1))^2 ... 
+    + (f(x, y-1) - g(x, y-1))^2 + (f(x, y) - g(x, y))^2 + (f(x, y+1) - g(x, y+1))^2 ... 
+    + (f(x+1, y-1) - g(x+1, y-1))^2 + (f(x+1, y) - g(x+1, y))^2 + (f(x+1, y+1) - g(x+1, y+1))^2;
+
+eqn1 = diff(epsilon, a) == 0;
+eqn2 = diff(epsilon, b) == 0;
+eqn3 = diff(epsilon, c) == 0;
+eqn4 = diff(epsilon, alpha) == 0;
+eqn5 = diff(epsilon, beta) == 0;
+eqn6 = diff(epsilon, gamma) == 0;
+
+coefficients = solve([eqn1, eqn2, eqn3, eqn4, eqn5, eqn6], [a, b, c, alpha, beta, gamma]);
+
+syms grad(x, y)
+grad(x, y) = sqrt((2*(coefficients.a)*x + (coefficients.c)*y + coefficients.alpha)^2 + (2*(coefficients.b)*y + (coefficients.c)*x + coefficients.beta)^2);
+
+originalImage = double(imread('cameraman.tif'));
+resultImage = zeros(size(originalImage), class(originalImage));
+
+% x is a row, y is a column
+for x = 2:(size(originalImage, 1) - 1)
+    for y = 2:(size(originalImage, 2) - 1)
+        resultImage(x, y) = sqrt((originalImage(x - 1, y - 1)/6 - y*(originalImage(x - 1, y - 1)/4 - originalImage(x - 1, y + 1)/4 - originalImage(x + 1, y - 1)/4 + originalImage(x + 1, y + 1)/4) + originalImage(x - 1, y + 1)/6 - originalImage(x + 1, y - 1)/6 - originalImage(x + 1, y + 1)/6 - x*(originalImage(x - 1, y - 1)/3 + originalImage(x - 1, y + 1)/3 + originalImage(x + 1, y - 1)/3 + originalImage(x + 1, y + 1)/3 - (2*originalImage(x, y))/3 - (2*originalImage(x, y - 1))/3 - (2*originalImage(x, y + 1))/3 + originalImage(x - 1, y)/3 + originalImage(x + 1, y)/3) - (2*x*originalImage(x, y))/3 - (2*x*originalImage(x, y - 1))/3 - (2*x*originalImage(x, y + 1))/3 + (x*originalImage(x - 1, y))/3 + (x*originalImage(x + 1, y))/3 + originalImage(x - 1, y)/6 - originalImage(x + 1, y)/6 + (x*originalImage(x - 1, y - 1))/3 + (x*originalImage(x - 1, y + 1))/3 + (x*originalImage(x + 1, y - 1))/3 + (x*originalImage(x + 1, y + 1))/3 + (y*originalImage(x - 1, y - 1))/4 - (y*originalImage(x - 1, y + 1))/4 - (y*originalImage(x + 1, y - 1))/4 + (y*originalImage(x + 1, y + 1))/4)^2 + (originalImage(x - 1, y - 1)/6 - x*(originalImage(x - 1, y - 1)/4 - originalImage(x - 1, y + 1)/4 - originalImage(x + 1, y - 1)/4 + originalImage(x + 1, y + 1)/4) - originalImage(x - 1, y + 1)/6 + originalImage(x + 1, y - 1)/6 - originalImage(x + 1, y + 1)/6 - y*(originalImage(x - 1, y - 1)/3 + originalImage(x - 1, y + 1)/3 + originalImage(x + 1, y - 1)/3 + originalImage(x + 1, y + 1)/3 - (2*originalImage(x, y))/3 + originalImage(x, y - 1)/3 + originalImage(x, y + 1)/3 - (2*originalImage(x - 1, y))/3 - (2*originalImage(x + 1, y))/3) - (2*y*originalImage(x, y))/3 + (y*originalImage(x, y - 1))/3 + (y*originalImage(x, y + 1))/3 - (2*y*originalImage(x - 1, y))/3 - (2*y*originalImage(x + 1, y))/3 + originalImage(x, y - 1)/6 - originalImage(x, y + 1)/6 + (x*originalImage(x - 1, y - 1))/4 - (x*originalImage(x - 1, y + 1))/4 - (x*originalImage(x + 1, y - 1))/4 + (x*originalImage(x + 1, y + 1))/4 + (y*originalImage(x - 1, y - 1))/3 + (y*originalImage(x - 1, y + 1))/3 + (y*originalImage(x + 1, y - 1))/3 + (y*originalImage(x + 1, y + 1))/3)^2);
+    end
+end
+
+figure;
+imshow(uint8(resultImage));
+
+figure;
+imshow(255 - uint8(resultImage)); % negative resultImage
